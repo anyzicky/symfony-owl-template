35f802dbc417bea25310d5eca817c219cbb3312e
any.zicky <any.zicky@gmail.com>
2016-07-05 19:40:05 +0300

    Инициализация проекта и заготовка шаблона

---
+++ src/Owl/SiteBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Owl\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('OwlSiteBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Owl/SiteBundle/OwlSiteBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Owl\SiteBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OwlSiteBundle extends Bundle
+{
+}
